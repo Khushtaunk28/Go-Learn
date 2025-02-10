57d7218317a5f5bf2d159429b899df2ecef552b9
Khushtaunk28 <Khush.taunk@gmail.com>
2025-02-10 12:24:48 +0530

    maps over

---
+++ Maps/maps.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// func main() {
+// 	websites := map[string]string{
+// 		"Google":     "google.com",
+// 		"Amazon web": "aws.com",
+// 	}
+// 	fmt.Println(websites)
+// 	fmt.Println(websites["Google"])
+// }
+
+func main(){
+	 
+}
