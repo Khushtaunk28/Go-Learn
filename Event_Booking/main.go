6b3f4c8dbc10427f13c12e64026eb092979da38a
Khushtaunk28 <Khush.taunk@gmail.com>
2025-02-22 18:41:30 +0530

    added post request /events to add events

--- Event_Booking/main.go
+++ Event_Booking/main.go
@@ -1,12 +1,16 @@
 package main
+
 import (
+	"Event_Booking/models"
 	"net/http"
-  
+
 	"github.com/gin-gonic/gin"
-  )
+)
 func main(){
 	server := gin.Default()
 	server.GET("/events",getEvents )
+
+	server.POST("/events",)
 
 
 
@@ -16,6 +20,19 @@ func main(){
 }
 
 func getEvents(context *gin.Context){
+	events:=models.GetAllEvents()
 	context.JSON(http.StatusOK,gin.H{"message":"Hello"})
+	context.JSON(http.StatusOK,events)
+}
 
+func createEvent(context *gin.Context){
+	var event models.Event
+	err:=context.ShouldBindJSON(&event)
+	if(err!=nil){
+		context.JSON(http.StatusBadRequest,gin.H{"message":"missing fields"})
+		return
+	}
+	event.ID=1
+	event.UserID=1
+	context.JSON(http.StatusCreated,gin.H{"message":"event created","event":event})
 }
