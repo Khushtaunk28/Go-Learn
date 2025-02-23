66c574ba156cc6cc8e17790956b608cc73730ff5
Khushtaunk28 <Khush.taunk@gmail.com>
2025-02-23 11:56:32 +0530

    connected to sql db, now the data is getting stored,used the tdm gcc for sqlite3

--- Event_Booking/main.go
+++ Event_Booking/main.go
@@ -1,38 +1,49 @@
 package main
 
 import (
+	"Event_Booking/db"
 	"Event_Booking/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
-func main(){
+
+func main() {
+	db.InitDB()
 	server := gin.Default()
-	server.GET("/events",getEvents )
+	server.GET("/events", getEvents)
 
-	server.POST("/events",)
-
-
+	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 
-
 }
 
-func getEvents(context *gin.Context){
-	events:=models.GetAllEvents()
-	context.JSON(http.StatusOK,gin.H{"message":"Hello"})
-	context.JSON(http.StatusOK,events)
-}
-
-func createEvent(context *gin.Context){
-	var event models.Event
-	err:=context.ShouldBindJSON(&event)
-	if(err!=nil){
-		context.JSON(http.StatusBadRequest,gin.H{"message":"missing fields"})
+func getEvents(context *gin.Context) {
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"msg": "couldnt fetch"})
 		return
 	}
-	event.ID=1
-	event.UserID=1
-	context.JSON(http.StatusCreated,gin.H{"message":"event created","event":event})
+	context.JSON(http.StatusOK, gin.H{"message": "Hello"})
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "missing fields"})
+		return
+	}
+	event.ID = 1
+	event.UserID = 1
+	err = event.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"msg": "couldnt create event"})
+		return
+	}
+	context.JSON(http.StatusCreated, gin.H{"message": "event created", "event": event})
+
 }
