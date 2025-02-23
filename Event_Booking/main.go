0f8fa7cd5ead37019430dae102cf61e20728e6df
Khushtaunk28 <Khush.taunk@gmail.com>
2025-02-23 12:57:48 +0530

    added get method for get event by id

--- Event_Booking/main.go
+++ Event_Booking/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"Event_Booking/db"
 	"Event_Booking/models"
+	"context"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,13 +14,13 @@ func main() {
 	db.InitDB()
 	server := gin.Default()
 	server.GET("/events", getEvents)
-
+	server.GET("/events/:id",getEvent)
 	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 
 }
-
+//get all events
 func getEvents(context *gin.Context) {
 	events, err := models.GetAllEvents()
 	if err != nil {
@@ -29,6 +31,29 @@ func getEvents(context *gin.Context) {
 	context.JSON(http.StatusOK, events)
 }
 
+//get event by id
+	func getEvent(context *gin.Context){
+		eventId,err:=strconv.ParseInt(context.Param("id"),10,64)
+		if err != nil {
+			context.JSON(http.StatusBadRequest, gin.H{"msg": "couldnt fetch eventid"})
+			return
+		}
+		event,err:=models.GetEventById(eventId)
+		if err != nil {
+			context.JSON(http.StatusInternalServerError, gin.H{"msg": "not a valid id"})
+			return
+		}
+		context.JSON(http.StatusOK,event)
+	}
+
+
+
+
+
+
+
+
+//post to create a new event
 func createEvent(context *gin.Context) {
 	var event models.Event
 	err := context.ShouldBindJSON(&event)
